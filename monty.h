65e2381a512bdefe7257bd9c8f29473aa3ef5750
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-17 10:04:43 +0100

    the header file

--- monty.h
+++ monty.h
@@ -40,8 +40,7 @@ typedef struct instruction_s
 
 /** monty function*/
 
-int execute_monty(FILE *monty_file);
-void (*get_func(char *opcode))(stack_t**, unsigned int);
+int execute(char *l);
 
 /**monty function option*/
 void push(stack_t **stack, unsigned int line_number);
@@ -51,6 +50,12 @@ void pop(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+
 
 
 
