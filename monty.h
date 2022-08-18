31cffc94a054da61844e6eaf0a6b0eb4f2520b1e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-18 16:18:35 +0100

    the header file

--- monty.h
+++ monty.h
@@ -46,9 +46,14 @@ typedef struct instruction_s
 
 /** monty function*/
 
-void execute(char *line, stack_t **stack, int line_number)
+void execute(char *line, stack_t **stack, int line_number);
 
 /**monty function option*/
+
+
+
+
+
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
@@ -61,6 +66,7 @@ void pchar(stack_t **stack, unsigned int line_number);
 void divide(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
+
 
 
 
