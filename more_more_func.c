2f2a9f54b184afaf2a726b16e2cf63357f0e4d37
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-18 23:23:54 +0100

    fun

---
+++ more_more_func.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+
+    (void)stack;
+	(void)line_number;
+}
+
+
+
+
+void mod(stack_t **stack, unsigned int line_number)
+{
+
+    (void)stack;
+	(void)line_number;
+}
