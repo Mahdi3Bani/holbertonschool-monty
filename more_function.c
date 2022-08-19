337cfc60bd4d11932d8beca5dd251386a3895302
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-19 23:43:54 +0100

    fix

--- more_function.c
+++ more_function.c
@@ -73,14 +73,14 @@ void pchar(stack_t **stack, unsigned int line_number)
 
 	if (!*stack)
 	{
-		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n >= 0 && (*stack)->n <= 127)
 			printf("%c\n", (*stack)->n);
 		else
 		{
-			fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+			fprintf(stderr, "L%lu: can't pchar, value out of range\n", line_number);
 			_free(*stack);
 			exit(EXIT_FAILURE);
 		}
