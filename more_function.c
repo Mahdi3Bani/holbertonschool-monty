04177ea3cc20dc443f08422ab7eb10c64d300636
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-19 22:40:18 +0100

    fixing

--- more_function.c
+++ more_function.c
@@ -25,7 +25,7 @@ void swap(stack_t **stack, unsigned int line_number)
 	if (val == NULL || val->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		clean_stack(stack);
+		_free(stack);
 		exit(EXIT_FAILURE);
 	}
 	else
